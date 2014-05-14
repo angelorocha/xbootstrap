ec44cbf72cc0f6249f4a4a4d87344d9c4765955f
Angelo Rocha <angelo.ofp@gmail.com>
2014-05-13 21:21:35 -0300

    add pt br lang

---
+++ language/pt-br_utf8/main.php
@@ -0,0 +1,14 @@
+<?php
+
+//Nav Menu
+define("THEME_HOME",            "Início");
+define("THEME_MODULE1",         "Sobre");
+define("THEME_MODULE2",         "Notícias");
+define("THEME_MODULE3",         "Contato");
+
+//Slider
+define("THEME_READMORE",        "Leia Mais");
+
+//Home Message
+define("THEME_ABOUTUS",         "Sobre Nós");
+define("THEME_LEARNINGMORE",    "Saiba Mais...");
