94af6b5fa442753cdf55c2989529227fe549f5cf
Angelo Rocha <angelo.ofp@gmail.com>
2014-05-13 21:17:37 -0300

    add language

---
+++ language/english/main.php
@@ -0,0 +1,14 @@
+<?php
+
+//Nav Menu
+define("THEME_HOME",        "Home");
+define("THEME_MODULE1",     "About");
+define("THEME_MODULE2",     "News");
+define("THEME_MODULE3",     "Contact");
+
+//Slider
+define("THEME_READMORE",    "Read More");
+
+//Home Message
+define("THEME_ABOUTUS",     "About Us");
+define("THEME_LEARNINGMORE",   "Learning More...");
