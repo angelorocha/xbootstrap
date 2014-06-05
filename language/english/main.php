f17d8b9ade1fada76398901fb7c708ce03d35b54
Angelo Rocha <angelo.ofp@gmail.com>
2014-06-04 22:14:26 -0300

    Started version 1.02
    - Add newbb template - Developing
    - Add language support
    - Some bug fix in PM template
    - Add search block template
    - Add new users template
    - Add top users template
    - Add XOOPS Code Style
    - Add XOOPS Quote Style
    - Add search block template
    - Add shot theme in block of theme select
    - Add top search box

--- language/english/main.php
+++ language/english/main.php
@@ -6,6 +6,8 @@ define("THEME_MODULE1", "About");
 define("THEME_MODULE2", "News");
 define("THEME_MODULE3", "Forum");
 define("THEME_MODULE4", "Contact");
+define("THEME_SEARCH_TEXT", "Search here...");
+define("THEME_SEARCH_BUTTON", "Go");
 
 //Slider
 define("THEME_READMORE", "Read More");
@@ -19,3 +21,5 @@ define("THEME_NEWBB_TOPIC", "More");
 define("THEME_FORUM_SPONSORBY", "Sponsor By.: ");
 define("THEME_GOTOTHEFORUM", "Go to Forum");
 define("THEME_FORUM_DESCRIPTION", "Description");
+
+
