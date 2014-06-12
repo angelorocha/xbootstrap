a1374ab19d69ac575ae11bad5ca0f765b06c19b1
Angelo Rocha <angelo.ofp@gmail.com>
2014-06-12 11:29:41 -0300

    update pt-br language

--- language/pt-br_utf8/main.php
+++ language/pt-br_utf8/main.php
@@ -4,7 +4,10 @@
 define("THEME_HOME", "Home");
 define("THEME_MODULE1", "About");
 define("THEME_MODULE2", "News");
-define("THEME_MODULE3", "Contact");
+define("THEME_MODULE3", "Forum");
+define("THEME_MODULE4", "Contact");
+define("THEME_SEARCH_TEXT", "Search here...");
+define("THEME_SEARCH_BUTTON", "Go");
 
 //Slider
 define("THEME_READMORE", "Read More");
@@ -15,4 +18,9 @@ define("THEME_LEARNINGMORE", "Learning More...");
 
 //NewBB
 define("THEME_NEWBB_TOPIC", "More");
-define("SPONSORBY", "Sponsor By.: ");
+define("THEME_FORUM_SPONSORBY", "Sponsor By.: ");
+define("THEME_GOTOTHEFORUM", "Go to Forum");
+define("THEME_FORUM_DESCRIPTION", "Description");
+define("THEME_NEWBB_SEARCH_FORUM", "Search in forum...");
+define("THEME_NEWBB_SEARCH_TOPIC", "Search in topic...");
+define("THEME_FORUM_DESC", "About This Forum");
