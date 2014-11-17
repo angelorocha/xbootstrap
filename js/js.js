af7bedf2b980c095dd8ab1286675cb0906eb9076
Angelo Rocha <angelo.ofp@gmail.com>
2014-11-17 17:04:03 -0300

    update newbb and profile templates

--- js/js.js
+++ js/js.js
@@ -31,9 +31,15 @@ jQuery(document).ready(function($){
     $(".xoopsform").find('select').addClass("form-control");
     $(".xoopsform").find('input[type="submit"]').addClass("btn btn-primary");
     $(".newbb-links").find('span').removeClass('forum_icon forum_button');
-});
-
-jQuery(document).ready(function($){
     $('.newbb-thread-attachment').find('br').remove();
     $('.newbb-thread-attachment').find('hr').remove();
 })
+
+/* Profile */
+jQuery(document).ready(function($){
+   $('#userinfo table, #regform table').addClass('table table-condensed table-hover').css('width','100%');
+   $('.profile-form input[type="text"], .profile-form input[type="password"]').addClass('form-control');
+   $('.profile-form select').addClass('form-control').css('width','100%');
+   $('.profile-form textarea').addClass('form-control');
+   $('.profile-form input.formButton').addClass('btn btn-primary pull-right');
+});
